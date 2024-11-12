51580c7e2c2feb2acae9025f73f00ea6f9400dc7
ArkoOf22 <arkodeepkoley123@gmail.com>
2024-11-12 18:17:44 +0530

    adding service logic

--- src/main/java/com/example/demojpa/controller/EmployeeController.java
+++ src/main/java/com/example/demojpa/controller/EmployeeController.java
@@ -2,7 +2,9 @@ package com.example.demojpa.controller;
 
 import com.example.demojpa.DTO.CreateEmployeeRequest;
 import com.example.demojpa.models.Employee;
+import com.example.demojpa.service.EmployeeService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmployeeController {
 
+    @Autowired
+    EmployeeService employeeService;
+
     @PostMapping("/createEmployee")
     public Employee createEmployee(@RequestBody @Valid CreateEmployeeRequest employee) {
-        return employee.toEmployee();
+
+        //Converting the DTO to the model
+        Employee newEmployee=employee.toEmployee();
+
+        employeeService.save(newEmployee);
+
+        return newEmployee;
     }
 
 }
--- src/main/java/com/example/demojpa/service/EmployeeService.java
+++ src/main/java/com/example/demojpa/service/EmployeeService.java
@@ -1,4 +1,15 @@
 package com.example.demojpa.service;
 
+
+import com.example.demojpa.DTO.CreateEmployeeRequest;
+import com.example.demojpa.models.Employee;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeService {
+    public void save(@Valid Employee employee) {
+
+
+    }
 }
