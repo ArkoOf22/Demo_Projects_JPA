b1fb7e756e35c7cfd7675de875fd47df1ca1f27a
ArkoOf22 <arkodeepkoley123@gmail.com>
2024-11-12 19:45:57 +0530

    Adding props and repository

--- src/main/java/com/example/demojpa/respository/employeeRepository.java
+++ src/main/java/com/example/demojpa/respository/employeeRepository.java
@@ -2,7 +2,9 @@ package com.example.demojpa.respository;
 
 import com.example.demojpa.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface employeeRepository extends JpaRepository<Employee, Integer> {
 
 }
